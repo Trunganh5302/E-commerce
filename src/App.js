bc24749934ae75ba3ee9db8541cd0fad779cd88a
TrungAnh <trunganh.edu.net@gmail.com>
2023-05-01 15:43:35 +0700

    login, signup base

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './Pages/Home/Home';
-import Shop from './Pages/Home/Shops';
+import Shop from './Pages/Shop/Shops';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/Signup/Signup';
+
 
 
 function App() {
@@ -11,6 +14,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/shop" element={<Shop />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/SignUp" element={<SignUp />} />
       </Routes>
     </Router>
   );
