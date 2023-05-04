6e212d3cc66ebb3399e0dd7e2abc8958a7904fae
TrungAnh <trunganh.edu.net@gmail.com>
2023-05-04 10:10:35 +0700

    test connect database

---
+++ seeders/20230504025931-demo-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   return queryInterface.bulkInsert('Users',[{
+    firstName: 'John',
+    lastName:'TrungAnh',
+    email:'trunganhno01@gmail.com',
+    createdAt: new Date(),
+    updatedAt: new Date()
+   }]);
+  },
+
+  // trong phần uo thì khi chạy bình thường 
+  // còn khi bị lỗi thì nó sẽ chạy phần down
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
